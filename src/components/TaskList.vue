c66b2048df51537ed065045f512117264a178085
João Marcos <jmarcossferreira@hotmail.com>
2022-09-23 13:18:06 -0300

    Fix import

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { Task } from "interfaces/task";
-import TaskItem from "TaskItem.vue";
+import TaskItem from "components/TaskItem.vue";
 
 defineProps<{ tasks: Task[] }>();
 </script>
